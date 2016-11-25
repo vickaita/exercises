6e0802468afce1ff9878cbfd93eb9466cfb17e2d
Vick Aita <vickaita@gmail.com>
2016-11-25 16:11:34 +0000

    Handle array directives

--- update/index.js
+++ update/index.js
@@ -1,16 +1,38 @@
 module.exports = function update(oldState, commands) {
-    const newState = {};
-    Object.keys(oldState).forEach(key => {
-        if (key in commands) {
-            const command = commands[key];
-            if ('$set' in command) {
-                newState[key] = command['$set'];
-            } else {
-                newState[key] = update(oldState[key], command);
-            }
-        } else {
-            newState[key] = oldState[key];
-        }
-    });
-    return newState;
+    // $apply works on any value
+    if ('$apply' in commands) {
+        return commands['$apply'](oldState);
+    }
+
+    // Otherwise operate on the correct type
+    if (Array.isArray(oldState)) {
+        return updateArray(oldState, commands);
+    } else if (typeof oldState === 'object') {
+        return updateObject(oldState, commands);
+    } else {
+        // TODO
+    }
 };
+
+function updateArray(oldState, commands) {
+    const command = Object.keys(commands)[0];
+    const args = commands[command];
+    const newState = oldState.concat();
+    switch (command) {
+        case '$push':
+            args.forEach(arg => Array.prototype.push.apply(newState, args));
+            break;
+        case '$splice':
+            args.forEach(arg => Array.prototype.splice.apply(newState, arg));
+            break;
+        case '$unshift':
+            args.forEach(arg => Array.prototype.unshift.apply(newState, args));
+            break;
+        default:
+            throw new Error('Unsupported operation');
+    }
+    return newState;
+}
+
+function updateObject(oldState, commands) {
+}
